e775eb4716a92a928a55185553e8e586dcec99a5
Jeris Manning <the.goose.moose@gmail.com>
2019-07-16 19:46:24 -0600

    Adds beginning of object

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,18 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+class Article {
+  constructor(story){
+    this.name = story;
+    this.title = story.title;
+    this.date = story.date;
+    this.firstParagraph = story.firstParagraph;
+    this.secondParagraph = story.secondParagraph;
+    this.thirdParagraph = story.thirdParagraph;
+  }
+}
+
+
+
+
+
